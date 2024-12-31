0a56d6c7d4b4084e7fbb2b31b9effac6ba7a7baf
Yahya Shannat <yahyashannat@gmail.com>
2024-12-31 17:16:22 +0300

    this is the end of day 31/12/2024

--- app/profile/create-post/page.tsx
+++ app/profile/create-post/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import BlogPostEditor from "./components/BlogPostEditor";
+import { createClient } from "@/lib/server-supabase";
+const page = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("auth.users").select("*");
+  if (error) {
+    console.error("Error fetching users:", error);
+  } else {
+    console.log("Users:", data);
+  }
 
-const page = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center mx-auto">
       <h1>Create Post</h1>
