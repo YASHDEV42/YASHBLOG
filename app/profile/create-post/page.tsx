4f09b369e3b480f10c669cbc21e7b609373ccb9a
Yahya Shannat <yahyashannat@gmail.com>
2025-01-02 21:33:51 +0300

    as string to the id when creating a post

--- app/profile/create-post/page.tsx
+++ app/profile/create-post/page.tsx
@@ -9,11 +9,12 @@ const page = async () => {
   if (!data?.user) {
     redirect("/login");
   }
+  console.log("User data from the server", data.user.id);
 
   return (
     <div className="w-full flex flex-col items-center justify-center mx-auto">
       <h1>Create Post</h1>
-      <BlogPostEditor user={data.user} />
+      <BlogPostEditor id={data.user.id as string} />
     </div>
   );
 };
