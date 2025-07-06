d4a01cf8b7c81691134297b83c4f02469860ff9f
Yahya Shannat <yahyashannat@gmail.com>
2025-07-07 01:47:31 +0300

    solving some repo problems

--- app/profile/create-post/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { CreatePostSkeleton } from "./components/create-post-skeleton";
-
-export default function page() {
-  return <CreatePostSkeleton />;
-}
--- app/profile/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { ProfilePageSkeleton } from "./components/profile-page-skeleton";
-
-export default function Loading() {
-  return <ProfilePageSkeleton />;
-}
